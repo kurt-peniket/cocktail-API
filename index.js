b491eb123bb4ba037f4a298b5bbb5fe04ae7ef91
kurt-peniket <64552537+kurt-peniket@users.noreply.github.com>
2020-09-10 16:54:54 +0100

    css changes

--- index.js
+++ index.js
@@ -8,20 +8,22 @@ $(document).ready(function(){
             $('#cocktail').empty();
             $('#cocktail').append(
                 `<div>
-                <h2>${data.drinks[0].strDrink}</h2>
-                <img src="${data.drinks[0].strDrinkThumb}">
-                <ul>
-                <li>${data.drinks[0].strIngredient1}</li>
-                <li>${data.drinks[0].strIngredient2}</li>
-                <li>${data.drinks[0].strIngredient3}</li>
-                <li>${data.drinks[0].strIngredient4}</li>
-                <li>${data.drinks[0].strIngredient5}</li>
-                <li>${data.drinks[0].strIngredient6}</li>
-                <li>${data.drinks[0].strIngredient7}</li>
-                <li>${data.drinks[0].strIngredient8}</li>
-                <li>${data.drinks[0].strIngredient9}</li>
-                <li>${data.drinks[0].strIngredient10}</li>
+                <h2><u>${data.drinks[0].strDrink}</u></h2>
+                <img src="${data.drinks[0].strDrinkThumb}" class="center-block img-responsive img-circle" alt="cocktail image">
+                <h3>Ingredients:</h3>
+                <ul class="list-group">
+                <li class="list-group-item list-group-item-info">${data.drinks[0].strIngredient1}</li>
+                <li class="list-group-item list-group-item-info">${data.drinks[0].strIngredient2}</li>
+                <li class="list-group-item list-group-item-info">${data.drinks[0].strIngredient3}</li>
+                <li class="list-group-item list-group-item-info">${data.drinks[0].strIngredient4}</li>
+                <li class="list-group-item list-group-item-info">${data.drinks[0].strIngredient5}</li>
+                <li class="list-group-item list-group-item-info">${data.drinks[0].strIngredient6}</li>
+                <li class="list-group-item list-group-item-info">${data.drinks[0].strIngredient7}</li>
+                <li class="list-group-item list-group-item-info">${data.drinks[0].strIngredient8}</li>
+                <li class="list-group-item list-group-item-info">${data.drinks[0].strIngredient9}</li>
+                <li class="list-group-item list-group-item-info">${data.drinks[0].strIngredient10}</li>
                 </ul>
+                <h3>Instructions:</h3>
                 <p>${data.drinks[0].strInstructions}</p>
                 </div>`
             );
